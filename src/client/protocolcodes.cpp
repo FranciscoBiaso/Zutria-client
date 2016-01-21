b5e9b8aaa19d1008a3786651d6f60833a79f317b
FranciscoBiaso <kikinhobiaso@gmail.com>
2016-01-20 23:13:39 -0200

    first commit

---
+++ src/client/protocolcodes.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2010-2015 OTClient <https://github.com/edubart/otclient>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "protocolcodes.h"
+
+namespace Proto {
+
+std::map<uint8, uint8> messageModesMap;
+
+void buildMessageModesMap(int version) {
+    messageModesMap.clear();
+
+    messageModesMap[Otc::MessageNone]                    = 0;
+    messageModesMap[Otc::MessageSay]                     = 1;
+    messageModesMap[Otc::MessageWhisper]                 = 2;
+    messageModesMap[Otc::MessageYell]                    = 3;
+    messageModesMap[Otc::MessagePrivateFrom]             = 4;
+    messageModesMap[Otc::MessagePrivateTo]               = 4;
+    messageModesMap[Otc::MessageChannel]                 = 5;
+    messageModesMap[Otc::MessageRVRChannel]              = 6;
+    messageModesMap[Otc::MessageRVRAnswer]               = 7;
+    messageModesMap[Otc::MessageRVRContinue]             = 8;
+    messageModesMap[Otc::MessageGamemasterBroadcast]     = 9;
+    messageModesMap[Otc::MessageGamemasterChannel]       = 10;
+    messageModesMap[Otc::MessageGamemasterPrivateFrom]   = 11;
+    messageModesMap[Otc::MessageGamemasterPrivateTo]     = 11;
+    messageModesMap[Otc::MessageChannelHighlight]        = 12;
+    // 13, 14, 15 ??
+    messageModesMap[Otc::MessageMonsterSay]              = 16;
+    messageModesMap[Otc::MessageMonsterYell]             = 17;
+    messageModesMap[Otc::MessageWarning]                 = 18;
+    messageModesMap[Otc::MessageGame]                    = 19;
+    messageModesMap[Otc::MessageLogin]                   = 20;
+    messageModesMap[Otc::MessageStatus]                  = 21;
+    messageModesMap[Otc::MessageLook]                    = 22;
+    messageModesMap[Otc::MessageFailure]                 = 23;
+    messageModesMap[Otc::MessageBlue]                    = 24;
+    messageModesMap[Otc::MessageRed]                     = 25;
+    messageModesMap[Otc::MessageLevelUp]                 = 26;
+
+}
+
+Otc::MessageMode translateMessageModeFromServer(uint8 mode)
+{
+    auto it = std::find_if(messageModesMap.begin(), messageModesMap.end(), [=] (const std::pair<uint8, uint8>& p) { return p.second == mode; });
+    if(it != messageModesMap.end())
+        return (Otc::MessageMode)it->first;
+    return Otc::MessageInvalid;
+}
+
+uint8 translateMessageModeToServer(Otc::MessageMode mode)
+{
+    if(mode < 0 || mode >= Otc::LastMessage)
+        return Otc::MessageInvalid;
+    auto it = messageModesMap.find(mode);
+    if(it != messageModesMap.end())
+        return it->second;
+    return Otc::MessageInvalid;
+}
+
+}
---
+++ vc12/Release/modules/game_skills/skills.lua
@@ -0,0 +1,239 @@
+skillsWindow = nil
+skillsButton = nil
+
+function init()
+  connect(LocalPlayer, {
+    onExperienceChange = onExperienceChange,
+    onLevelChange = onLevelChange,    
+    onSkillChange = onSkillChange,
+    onLevelPointsChange = onLevelPointsChange,
+  })
+  connect(g_game, {
+    onGameStart = refresh,
+    onGameEnd = offline,    
+  })
+
+  skillsButton = modules.client_topmenu.addRightGameToggleButton('skillsButton', tr('Skills') .. ' (Ctrl+S)', '/images/topbuttons/skills', toggle)
+  skillsButton:setOn(true)
+  skillsWindow = g_ui.loadUI('skills', modules.game_interface.getRightPanel())
+
+  g_keyboard.bindKeyDown('Ctrl+S', toggle)
+
+  refresh()
+  skillsWindow:setup()
+
+end
+
+function terminate()
+  disconnect(LocalPlayer, {
+    onExperienceChange = onExperienceChange,
+    onLevelChange = onLevelChange,
+    onSkillChange = onSkillChange,
+    onLevelPointsChange = onLevelPointsChange
+  })
+  
+  disconnect(g_game, {
+    onGameStart = refresh,
+    onGameEnd = offline,  
+  })
+
+  g_keyboard.unbindKeyDown('Ctrl+S')
+  skillsWindow:destroy()
+  skillsButton:destroy()
+end
+
+function expForLevel(level)
+  return math.floor((50*level*level*level)/3 - 100*level*level + (850*level)/3 - 200)
+end
+
+function expToAdvance(currentLevel, currentExp)
+  return expForLevel(currentLevel+1) - currentExp
+end
+
+function resetSkillColor(id)
+  local skill = skillsWindow:recursiveGetChildById(id)
+  local widget = skill:getChildById('value')
+  widget:setColor('#bbbbbb')
+end
+
+function setSkillValue(id, value)
+  local skill = skillsWindow:recursiveGetChildById(id)
+  local widget = skill:getChildById('value')
+  widget:setText(value)
+end
+
+function setSkillColor(id, value)
+  local skill = skillsWindow:recursiveGetChildById(id)
+  local widget = skill:getChildById('value')
+  widget:setColor(value)
+end
+
+function setSkillTooltip(id, value)
+  local skill = skillsWindow:recursiveGetChildById(id)
+  local widget = skill:getChildById('value')
+  widget:setTooltip(value)
+end
+
+function setSkillPercent(id, percent, tooltip)
+  local skill = skillsWindow:recursiveGetChildById(id)
+  local widget = skill:getChildById('percent')
+  widget:setPercent(math.floor(percent))
+
+  if tooltip then
+    widget:setTooltip(tooltip)
+  end
+end
+
+function checkAlert(id, value, maxValue, threshold, greaterThan)
+  if greaterThan == nil then greaterThan = false end
+  local alert = false
+
+  -- maxValue can be set to false to check value and threshold
+  -- used for regeneration checking
+  if type(maxValue) == 'boolean' then
+    if maxValue then
+      return
+    end
+
+    if greaterThan then
+      if value > threshold then
+        alert = true
+      end
+    else
+      if value < threshold then
+        alert = true
+      end
+    end
+  elseif type(maxValue) == 'number' then
+    if maxValue < 0 then
+      return
+    end
+
+    local percent = math.floor((value / maxValue) * 100)
+    if greaterThan then
+      if percent > threshold then
+        alert = true
+      end
+    else
+      if percent < threshold then
+        alert = true
+      end
+    end
+  end
+
+  if alert then
+    setSkillColor(id, '#b22222') -- red
+  else
+    resetSkillColor(id)
+  end
+end
+
+function refresh()
+  local player = g_game.getLocalPlayer()
+  if not player then return end
+  
+  local contentsPanel = skillsWindow:getChildById('contentsPanel')
+  skillsWindow:setContentMinimumHeight(44)
+  skillsWindow:setContentMaximumHeight(300)
+end
+
+function toggle()
+  if skillsButton:isOn() then
+    skillsWindow:close()
+    skillsButton:setOn(false)
+  else
+    skillsWindow:open()
+    skillsButton:setOn(true)
+  end
+end
+
+function onMiniWindowClose()
+  skillsButton:setOn(false)
+end
+
+function onSkillButtonClick(button)
+  local percentBar = button:getChildById('percent')
+  if percentBar then
+    percentBar:setVisible(not percentBar:isVisible())
+    if percentBar:isVisible() then
+      button:setHeight(23)
+    else
+      button:setHeight(23 - 6)
+    end
+  end
+end
+
+function onAddSkillButtonClick(button)
+  local player = g_game.getLocalPlayer()
+  if not player then return end 
+  
+  local widgetParent = button:getParent()
+  local idLabel = widgetParent:getId()
+  local playerId = player:getId()
+  
+  if idLabel == 'health' then
+    g_game.addSkillPoint(playerId, GameSkills.Health, 1) 
+  elseif idLabel == 'physicalAttack' then
+    g_game.addSkillPoint(playerId, GameSkills.PhysicalAttack, 1) 
+  elseif idLabel == 'physicalDefense' then
+    g_game.addSkillPoint(playerId, GameSkills.PhysicalDefense, 1)
+  elseif idLabel == 'capacity' then
+    g_game.addSkillPoint(playerId, GameSkills.Capacity, 1)
+  elseif idLabel == 'manaPoints' then
+    g_game.addSkillPoint(playerId, GameSkills.ManaPoints, 1)
+  elseif idLabel == 'magicAttack' then
+    g_game.addSkillPoint(playerId, GameSkills.MagicAttack, 1)
+  elseif idLabel == 'magicDefense' then
+    g_game.addSkillPoint(playerId, GameSkills.MagicDefense, 1)
+  elseif idLabel == 'magicPoints' then
+    g_game.addSkillPoint(playerId, GameSkills.MagicPoints, 1)
+  elseif idLabel == 'playerSpeed' then
+    g_game.addSkillPoint(playerId, GameSkills.PlayerSpeed, 1)
+  elseif idLabel == 'attackSpeed' then
+    g_game.addSkillPoint(playerId, GameSkills.AttackSpeed, 1)
+  elseif idLabel == 'cooldown' then
+    g_game.addSkillPoint(playerId, GameSkills.Cooldown, 1)
+  elseif idLabel == 'avoidance' then
+    g_game.addSkillPoint(playerId, GameSkills.Avoidance, 1)
+  end
+end
+
+function onExperienceChange(localPlayer, value)
+  setSkillValue('experience', value)
+end
+
+function onLevelChange(localPlayer, level, percent, oldLevel)
+  setSkillValue('level', level)
+  local text = tr('You have %s percent to go', 100 - percent) .. '\n' ..
+               tr('%s of experience left', expToAdvance(localPlayer:getLevel(), localPlayer:getExperience()))
+
+  if localPlayer.expSpeed ~= nil then
+     local expPerHour = math.floor(localPlayer.expSpeed * 3600)
+     if expPerHour > 0 then
+        local nextLevelExp = expForLevel(localPlayer:getLevel()+1)
+        local hoursLeft = (nextLevelExp - localPlayer:getExperience()) / expPerHour
+        local minutesLeft = math.floor((hoursLeft - math.floor(hoursLeft))*60)
+        hoursLeft = math.floor(hoursLeft)
+        text = text .. '\n' .. tr('%d of experience per hour', expPerHour)
+        text = text .. '\n' .. tr('Next level in %d hours and %d minutes', hoursLeft, minutesLeft)
+     end
+  end
+
+  setSkillPercent('level', percent, text)
+ -- if oldLevel < level then
+  --  setSkillValue('levelPoints', level)
+  --end
+end
+
+
+function onSpeedChange(localPlayer, speed)
+  --setSkillValue('playerSpeed', speed)
+end
+
+function onSkillChange(localPlayer, id, skill, oldskill)
+    setSkillValue(GameSkillsName[id + 1], skill)
+end
+
+function onLevelPointsChange(localPlayer, levelPoints)
+  setSkillValue('levelPoints', levelPoints)
+end
